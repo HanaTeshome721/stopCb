db26b0662c7a43ed38d1f8a52cc9ec69d5d50e44
HanaTeshome721 <hana2729tm@gmail.com>
2025-12-04 13:48:30 +0300

    feat: test route to check modularity

---
+++ src/routes/test_route.ts
@@ -0,0 +1,11 @@
+// import { Router ,Request,Response } from "express";
+// import User from "../database/models/usermodel"
+
+// const router = Router();
+
+// router.get("/test",async( req:Request,res:Response)=>{
+//     const users = await User.find();
+//     res.json(users)
+// });
+
+// export default router;
